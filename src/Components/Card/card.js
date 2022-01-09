ba02d1f982b6651e9571b4900fe457c62f37765a
Jen <jenscrivener3@gmail.com>
2022-01-09 19:12:13 +1100

    Task tracker app

---
+++ src/Components/Card/card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Card = ({ listOfTodos }) => {
+    return(
+        <>
+            {listOfTodos.map(todo => {
+                return(
+                    <ul key = {todo.id}>
+                        <li>{todo.content}</li>
+                    </ul>
+                )
+            })}
+        </>
+    )
+}
